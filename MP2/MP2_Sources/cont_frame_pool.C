25fbcd0eeeae0d4d0101249db330e47ad2c55fd6
anminfang <anminfang@tamu.edu>
2025-01-30 13:42:57 -0600

    added free list and buddy system

--- MP2/MP2_Sources/cont_frame_pool.C
+++ MP2/MP2_Sources/cont_frame_pool.C
@@ -103,6 +103,7 @@
 #include "console.H"
 #include "utils.H"
 #include "assert.H"
+#include "frame_list.H"
 
 /*--------------------------------------------------------------------------*/
 /* DATA STRUCTURES */
@@ -126,48 +127,137 @@
 /* METHODS FOR CLASS   C o n t F r a m e P o o l */
 /*--------------------------------------------------------------------------*/
 
+// Initialize static member
+ContFramePool *ContFramePool::first_pool = nullptr;
+
 ContFramePool::ContFramePool(unsigned long _base_frame_no,
                              unsigned long _n_frames,
                              unsigned long _info_frame_no)
 {
     Console::puts("ContframePool::Constructor implemented!\n");
+    Console::puts("base_frame_no: ");
+    Console::puti(_base_frame_no);
+    Console::puts("\n");
+    Console::puts("n_frames: ");
+    Console::puti(_n_frames);
+    Console::puts("\n");
+    Console::puts("info_frame_no: ");
+    Console::puti(_info_frame_no);
+    Console::puts("\n");
+
     base_frame_no = _base_frame_no;
     nframes = _n_frames;
     info_frame_no = _info_frame_no;
-    if (_info_frame_no == 0)
+    allocated_blocks = nullptr;
+    allocated_block_offset = 0;
+
+    // Step 1: figure out how many frames are needed for metadata
+    unsigned long needed = needed_info_frames(nframes);
+
+    // Step 2: decide where to place the info frames in the pool
+    unsigned long info_start; // We'll store the first metadata frame
+    if (info_frame_no == 0)
     {
-        free_list.init(_base_frame_no + 1, _n_frames - 1);
+        // Step 3: We place the metadata frames at the front
+        info_start = base_frame_no;
+        // Step 4: Then remove those frames from the "real" pool
+        base_frame_no += needed;
+        nframes -= needed;
     }
     else
     {
-        free_list.init(_base_frame_no, _n_frames - _info_frame_no);
+        // Step 5: If the user gave us an explicit frame for metadata
+        info_start = info_frame_no;
     }
+
+    void *free_list_storage = (void *)((unsigned long)info_start * Machine::PAGE_SIZE);
+
+    free_list.init_managed_region(base_frame_no, nframes, free_list_storage);
+
+    unsigned long frame_list_usage = sizeof(FrameList::FreeBlock);
+
+    allocated_block_offset = frame_list_usage;
+
+    next_pool = first_pool;
+    first_pool = this;
 }
 
 unsigned long ContFramePool::get_frames(unsigned int _n_frames)
 {
-    Console::puts("ContframePool::get_frames implemented!\n");
-    return free_list.allocate(_n_frames);
+    // Step 1: Allocate frames from the free_list
+    unsigned long frame_no = free_list.allocate(_n_frames);
+    if (frame_no == 0)
+    {
+        return 0; // fails
+    }
+
+    // Step 2: Place an AllocatedBlock in the metadata region
+    AllocatedBlock *new_block =
+        (AllocatedBlock *)((info_frame_no * Machine::PAGE_SIZE) + allocated_block_offset);
+
+    // Step 3: Store frame_no as relative or absolute, but must be consistent in release_frames().
+    new_block->frame_no = frame_no - base_frame_no; // store relative
+    new_block->length = _n_frames;
+    new_block->next = allocated_blocks;
+    allocated_blocks = new_block;
+
+    allocated_block_offset += sizeof(AllocatedBlock);
+
+    // Step 4: Return absolute frame number
+    return frame_no;
 }
 
-void ContFramePool::mark_inaccessible(unsigned long _base_frame_no,
-                                      unsigned long _n_frames)
+void ContFramePool::mark_inaccessible(unsigned long _base_frame_no, unsigned long _n_frames)
 {
-    // Mark frames as unavailable in free list
-    free_list.mark_unavailable(_base_frame_no - base_frame_no, _n_frames);
+    free_list.mark_unavailable(_base_frame_no, _n_frames);
 }
 
 void ContFramePool::release_frames(unsigned long _first_frame_no)
 {
-    // Find the pool that owns this frame
-    // Then release frames back to that pool's free list
-    free_list.release(_first_frame_no - base_frame_no, 1); // Start with 1 frame
+    // Step 1: Find which ContFramePool this frame belongs to
+    ContFramePool *pool = first_pool;
+    while (pool)
+    {
+        unsigned long start = pool->base_frame_no;
+        unsigned long end_plus = start + pool->nframes;
+
+        if (_first_frame_no >= start && _first_frame_no < end_plus)
+        {
+            // Found the correct pool
+            AllocatedBlock **curr = &pool->allocated_blocks;
+            while (*curr)
+            {
+                if ((*curr)->frame_no == (_first_frame_no - start))
+                {
+                    unsigned int length = (*curr)->length;
+                    AllocatedBlock *dead = *curr;
+                    *curr = (*curr)->next;
+
+                    // Step 2: Return frames to free_list
+                    pool->free_list.release(_first_frame_no, length);
+                    return;
+                }
+                curr = &((*curr)->next);
+            }
+            Console::puts("Warning: Could not find allocation record for frame\n");
+            return;
+        }
+        pool = pool->next_pool;
+    }
 }
 
 unsigned long ContFramePool::needed_info_frames(unsigned long _n_frames)
 {
-    // Calculate frames needed for free list management
-    // Each FreeBlock needs sizeof(FreeBlock) bytes
-    unsigned long bytes_needed = sizeof(FreeBlock) * _n_frames; // Worst case
-    return (bytes_needed + FRAME_SIZE - 1) / FRAME_SIZE;        // Round up
+    // Step 1: Calculate the number of bytes needed for metadata
+    unsigned long block_bytes =
+        (sizeof(FrameList::FreeBlock) + sizeof(AllocatedBlock)) * _n_frames;
+
+    // Step 2: Convert bytes to frames
+    unsigned long frames = (block_bytes + FRAME_SIZE - 1) / FRAME_SIZE;
+
+    if (frames == 0)
+    {
+        frames = 1;
+    }
+    return frames;
 }
--- MP2/MP2_Sources/kernel.C
+++ MP2/MP2_Sources/kernel.C
@@ -11,13 +11,12 @@
 
 */
 
-
 /*--------------------------------------------------------------------------*/
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-#define MB * (0x1 << 20)
-#define KB * (0x1 << 10)
+#define MB *(0x1 << 20)
+#define KB *(0x1 << 10)
 /* Makes things easy to read */
 
 #define KERNEL_POOL_START_FRAME ((2 MB) / (4 KB))
@@ -43,23 +42,25 @@
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
 
-#include "machine.H"     /* LOW-LEVEL STUFF   */
+#include "machine.H" /* LOW-LEVEL STUFF   */
 #include "console.H"
 
 #include "assert.H"
-#include "cont_frame_pool.H"  /* The physical memory manager */
+#include "cont_frame_pool.H" /* The physical memory manager */
 
 /*--------------------------------------------------------------------------*/
 /* FORWARDS */
 /*--------------------------------------------------------------------------*/
 
-void test_memory(ContFramePool * _pool, unsigned int _allocs_to_go);
-
+void test_memory(ContFramePool *_pool, unsigned int _allocs_to_go);
+void failed_test_frame_pool();
+void successful_test_frame_pool();
 /*--------------------------------------------------------------------------*/
 /* MAIN ENTRY INTO THE OS */
 /*--------------------------------------------------------------------------*/
 
-int main() {
+int main()
+{
 
     Console::init();
     Console::redirect_output(true); // comment if you want to stop redirecting qemu window output to stdout
@@ -67,70 +68,117 @@ int main() {
     /* -- INITIALIZE FRAME POOLS -- */
 
     /* ---- KERNEL POOL -- */
-    
+
     ContFramePool kernel_mem_pool(KERNEL_POOL_START_FRAME,
                                   KERNEL_POOL_SIZE,
                                   0);
-    
+
     /* ---- PROCESS POOL -- */
 
-/*  // In later machine problems, we will be using two pools. You may want to comment this out and test 
-    // the management of two pools.
+    /*  // In later machine problems, we will be using two pools. You may want to comment this out and test
+        // the management of two pools.
 
-    unsigned long n_info_frames = ContFramePool::needed_info_frames(PROCESS_POOL_SIZE);
+        unsigned long n_info_frames = ContFramePool::needed_info_frames(PROCESS_POOL_SIZE);
 
-    unsigned long process_mem_pool_info_frame = kernel_mem_pool.get_frames(n_info_frames);
-    
-    ContFramePool process_mem_pool(PROCESS_POOL_START_FRAME,
-                                   PROCESS_POOL_SIZE,
-                                   process_mem_pool_info_frame);
-    
-    process_mem_pool.mark_inaccessible(MEM_HOLE_START_FRAME, MEM_HOLE_SIZE);
-*/
+        unsigned long process_mem_pool_info_frame = kernel_mem_pool.get_frames(n_info_frames);
+
+        ContFramePool process_mem_pool(PROCESS_POOL_START_FRAME,
+                                       PROCESS_POOL_SIZE,
+                                       process_mem_pool_info_frame);
+
+        process_mem_pool.mark_inaccessible(MEM_HOLE_START_FRAME, MEM_HOLE_SIZE);
+    */
 
     /* -- MOST OF WHAT WE NEED IS SETUP. THE KERNEL CAN START. */
 
     Console::puts("Hello World!\n");
 
     /* -- TEST MEMORY ALLOCATOR */
-    
+
     test_memory(&kernel_mem_pool, N_TEST_ALLOCATIONS);
 
     /* ---- Add code here to test the frame pool implementation. */
-    
+
     /* -- NOW LOOP FOREVER */
     Console::puts("Testing is DONE. We will do nothing forever\n");
     Console::puts("Feel free to turn off the machine now.\n");
+    Console::puts("==============================================\n");
 
-    for(;;);
+    failed_test_frame_pool();
+    successful_test_frame_pool();
+
+    for (;;)
+        ;
 
     /* -- WE DO THE FOLLOWING TO KEEP THE COMPILER HAPPY. */
     return 1;
 }
 
-void test_memory(ContFramePool * _pool, unsigned int _allocs_to_go) {
-    Console::puts("alloc_to_go = "); Console::puti(_allocs_to_go); Console::puts("\n");
-    if (_allocs_to_go > 0) {
-        // We have not reached the end yet. 
-        int n_frames = _allocs_to_go % 4 + 1;               // number of frames you want to allocate
-        unsigned long frame = _pool->get_frames(n_frames);  // we allocate the frames from the pool
-        int * value_array = (int*)(frame * (4 KB));         // we pick a unique number that we want to write into the memory we just allocated
-        for (int i = 0; i < (1 KB) * n_frames; i++) {       // we write this value int the memory locations
+void test_memory(ContFramePool *_pool, unsigned int _allocs_to_go)
+{
+    Console::puts("alloc_to_go = ");
+    Console::puti(_allocs_to_go);
+    Console::puts("\n");
+    if (_allocs_to_go > 0)
+    {
+        // We have not reached the end yet.
+        int n_frames = _allocs_to_go % 4 + 1;              // number of frames you want to allocate
+        unsigned long frame = _pool->get_frames(n_frames); // we allocate the frames from the pool
+        int *value_array = (int *)(frame * (4 KB));        // we pick a unique number that we want to write into the memory we just allocated
+        for (int i = 0; i < (1 KB) * n_frames; i++)
+        { // we write this value int the memory locations
             value_array[i] = _allocs_to_go;
         }
-        test_memory(_pool, _allocs_to_go - 1);              // recursively allocate and uniquely mark more memory
-        for (int i = 0; i < (1 KB) * n_frames; i++) {       // We check the values written into the memory before we recursed 
-            if(value_array[i] != _allocs_to_go){            // If the value stored in the memory locations is not the same that we wrote a few lines above
-                                                            // then somebody overwrote the memory.
+        test_memory(_pool, _allocs_to_go - 1); // recursively allocate and uniquely mark more memory
+        for (int i = 0; i < (1 KB) * n_frames; i++)
+        { // We check the values written into the memory before we recursed
+            if (value_array[i] != _allocs_to_go)
+            { // If the value stored in the memory locations is not the same that we wrote a few lines above
+              // then somebody overwrote the memory.
                 Console::puts("MEMORY TEST FAILED. ERROR IN FRAME POOL\n");
-                Console::puts("i ="); Console::puti(i);
-                Console::puts("   v = "); Console::puti(value_array[i]); 
-                Console::puts("   n ="); Console::puti(_allocs_to_go);
+                Console::puts("i =");
+                Console::puti(i);
+                Console::puts("   v = ");
+                Console::puti(value_array[i]);
+                Console::puts("   n =");
+                Console::puti(_allocs_to_go);
                 Console::puts("\n");
-                for(;;);                                    // We throw a fit.
+                for (;;)
+                    ; // We throw a fit.
             }
         }
-        ContFramePool::release_frames(frame);               // We free the memory that we allocated above.
+        ContFramePool::release_frames(frame);
     }
 }
 
+// Failed test case for frame pool
+void failed_test_frame_pool()
+{
+    Console::puts("Failed test case for frame pool\n");
+    ContFramePool frame_pool(0, 10, 0);
+    frame_pool.mark_inaccessible(0, 10);
+    Console::puts("frame_pool.get_frames(1) returned: ");
+    Console::puti(frame_pool.get_frames(1));
+    Console::puts("\n");
+    frame_pool.release_frames(0);
+    Console::puts("==============================================\n");
+}
+
+// Successful test case for frame pool
+void successful_test_frame_pool()
+{
+    Console::puts("Successful test case for frame pool\n");
+
+    ContFramePool frame_pool(10, 10, 0);
+    frame_pool.mark_inaccessible(10, 5);
+
+    Console::puts("frame_pool.get_frames(1) returned: ");
+    unsigned long frame = frame_pool.get_frames(1); // Should succeed, using frame 15-19
+    Console::puti(frame);
+    Console::puts("\n");
+    if (frame != 0)
+    {
+        frame_pool.release_frames(frame);
+    }
+    Console::puts("==============================================\n");
+}
